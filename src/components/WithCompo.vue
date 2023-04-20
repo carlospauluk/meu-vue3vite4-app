68c45d2e2de03c9121970e52ccb956102ffbc80e
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2023-04-20 08:37:52 -0300

    ...

---
+++ src/components/WithCompo.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Aqui vai ter um compo da minha-biblioteca</h1>
+
+    <MeuCompo />
+</template>
+
+<script>
+import MeuCompo from "minha-biblioteca";
+export default {
+    name: "WithCompo",
+    components: {
+        MeuCompo,
+    },
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+
+};
+</script>
